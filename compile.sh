03a57e5e0aeb3ce5766c2702710713e2c1b1a638
Anthony Esber <anthony.esber@student.upt.ro>
2023-05-13 16:52:05 +0300

    Final touch-up for the project

--- compile.sh
+++ compile.sh
@@ -1,13 +1,11 @@
+# Do not edit this script !!!
 file="$1"
-
 if test ! -f $1
-then echo "Not a regular file"
-exit 1
+then exit 1
 else
-output=$(gcc -Wall -Wextra "$file" 2>&1)
+output=$(gcc -Wall -Wextra "$file" -o /dev/null 2>&1)
 errors=$(echo "$output" | grep -c 'error')
 warns=$(echo "$output" | grep -c 'warning')
-
 echo "$errors"
 echo "$warns"
 fi 
